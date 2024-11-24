9e7afd194a1f31ae2bae1ae29945833172bb8011
Sebastian Engel <sebastianengel@posteo.de>
2024-11-24 23:55:34 +0100

    feat: reference actual restaurant object in apple maps

--- RestaurantReviews/Views/RestaurantDetailView.swift
+++ RestaurantReviews/Views/RestaurantDetailView.swift
@@ -104,6 +104,7 @@ private struct PhotosGridView: View {
 private struct MapPreview: View {
     let restaurant: Restaurant
     @State private var cameraPosition: MapCameraPosition
+    @State private var mapItem: MKMapItem?
     
     init(restaurant: Restaurant) {
         self.restaurant = restaurant
@@ -134,13 +135,57 @@ private struct MapPreview: View {
         .onTapGesture {
             openInMaps()
         }
+        .task {
+            await findMapItem()
+        }
+    }
+    
+    private func findMapItem() async {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = restaurant.name
+        request.region = MKCoordinateRegion(
+            center: restaurant.coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        )
+        
+        do {
+            let search = MKLocalSearch(request: request)
+            let response = try await search.start()
+            
+            // Find the closest match
+            let closestItem = response.mapItems.min { item1, item2 in
+                let distance1 = item1.placemark.coordinate.distance(to: restaurant.coordinate)
+                let distance2 = item2.placemark.coordinate.distance(to: restaurant.coordinate)
+                return distance1 < distance2
+            }
+            
+            if let item = closestItem {
+                mapItem = item
+            }
+        } catch {
+            print("Failed to find map item: \(error)")
+        }
     }
     
     private func openInMaps() {
-        let placemark = MKPlacemark(coordinate: restaurant.coordinate)
-        let mapItem = MKMapItem(placemark: placemark)
-        mapItem.name = restaurant.name
-        mapItem.openInMaps()
+        if let mapItem = mapItem {
+            mapItem.openInMaps()
+        } else {
+            // Fallback to simple coordinates if we couldn't find the place
+            let placemark = MKPlacemark(coordinate: restaurant.coordinate)
+            let fallbackItem = MKMapItem(placemark: placemark)
+            fallbackItem.name = restaurant.name
+            fallbackItem.openInMaps()
+        }
+    }
+}
+
+// Add extension to help calculate distances between coordinates
+extension CLLocationCoordinate2D {
+    func distance(to other: CLLocationCoordinate2D) -> CLLocationDistance {
+        let this = CLLocation(latitude: latitude, longitude: longitude)
+        let that = CLLocation(latitude: other.latitude, longitude: other.longitude)
+        return this.distance(from: that)
     }
 }
 
