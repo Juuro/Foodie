cf1deb6c1d1bde0daa746b4fee68c959042669da
Sebastian Engel <sebastianengel@posteo.de>
2024-11-24 20:48:34 +0100

    Initial Commit

---
+++ RestaurantReviewsTests/RestaurantReviewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantReviewsTests.swift
+//  RestaurantReviewsTests
+//
+//  Created by Sebastian Engel on 24.11.24.
+//
+
+import Testing
+@testable import RestaurantReviews
+
+struct RestaurantReviewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
